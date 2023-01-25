48c0ae1db728cc55343a3da89cdbea58741f1b1f
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-01-25 11:14:03 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -16,7 +16,7 @@ class Home extends Component {
               className="banner-img"
             />
             <p className="para">
-              Fashion is a part of the daily air and it does not quite help that
+              Fashion is part of the daily air and it does not quite help that
               it changes all the time. Clothes have always been a marker of the
               era and we are in a revolution. Your fashion makes you been seen
               and heard that way you are. So, celebrate the seasons new and
